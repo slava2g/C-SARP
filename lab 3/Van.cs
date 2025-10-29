1ba5b2c78c2bee49ae188016ad0d282f79d8ef02
slava2g <slava21112005@gmail.com>
2025-10-29 13:58:37 +0200

    Add files via upload

---
+++ lab 3/Van.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliverySystem
+{
+    public class Van : Car
+    {
+        private double loadCapacity;
+        private double currentLoad;
+
+        // ВАЖНО: вызываем protected-конструктор Car с параметром maxSpeed = 140.0
+        public Van(string brand, int year, double mileage, int doors, double loadCapacity)
+            : base(brand, year, mileage, doors, 140.0)
+        {
+            this.loadCapacity = loadCapacity;
+            this.currentLoad = 0;
+        }
+
+        public override string GetInfo()
+        {
+            // doors и fuelLevel доступны тут, потому что они protected в Car
+            return $"Van: {brand} ({year}), Doors: {doors}, Load: {currentLoad}/{loadCapacity}kg, Fuel: {fuelLevel}L";
+        }
+
+        public void LoadCargo(double weight)
+        {
+            if (currentLoad + weight <= loadCapacity)
+            {
+                currentLoad += weight;
+                Console.WriteLine($"{weight} kg loaded into the van.");
+            }
+            else
+            {
+                Console.WriteLine("Too heavy! Cannot load more cargo.");
+            }
+        }
+
+        public void UnloadCargo()
+        {
+            currentLoad = 0;
+            Console.WriteLine("Van unloaded.");
+        }
+    }
+}
