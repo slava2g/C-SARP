0e75a1631f4c1b1b6fd4dc8c105ca44f49a6b6fe
slava2g <slava21112005@gmail.com>
2025-12-04 22:23:25 +0200

    Add files via upload

---
+++ lab5/OrderPunkt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class OrderPunkt
+    {
+        public MenuPunkt Punkt { get; set; }
+        public int quantity; 
+
+        public decimal Total => Punkt.Price * quantity; 
+        public override string ToString() 
+        {
+            return $"{Punkt.Name} x {quantity} = {Total} грн";
+        }
+    }
+}
